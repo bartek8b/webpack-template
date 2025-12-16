e6a11df8871a77fe1ea61a7b5c381e7fed0b1a55
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-12-16 20:56:35 +0100

    Set config for using jest and babel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,9 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.jest },
+    },
     ignores: ['node_modules/', 'dist/'],
   },
   {
