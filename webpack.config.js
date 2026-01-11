94c2fb0e319eaee50eee7899c403aabe67e78ba4
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-01-11 13:09:41 +0100

    Change style-loader to mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -19,12 +20,13 @@ module.exports = {
       // Uncomment the below line to set the path for favicon
       // favicon: './src/favicon.png',
     }),
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.html$/i,
